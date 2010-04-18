125862e4ff49f2021b19f1e4c6ec4863a370e765
void <git.email.address@googlemail.com>
2010-04-18 22:03:54 +0200

    I have no idea what I changed there

--- nil/http.py
+++ nil/http.py
@@ -1,6 +1,11 @@
 import urllib2
 
 def download(url):
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	data = response.read()
+	return data
+	
 	try:
 		request = urllib2.Request(url)
 		response = urllib2.urlopen(request)
